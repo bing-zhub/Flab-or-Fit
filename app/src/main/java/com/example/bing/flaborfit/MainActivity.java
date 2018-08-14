5864dd7e602494f84d0d1ae111e85057938fc7c4
bing.zhub <bing.zhub@gmail.com>
2018-08-14 11:03:21 +0800

    starting function

--- app/src/main/java/com/example/bing/flaborfit/MainActivity.java
+++ app/src/main/java/com/example/bing/flaborfit/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.bing.flaborfit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout weightBtn = (RelativeLayout)findViewById(R.id.weightBtn);
+        RelativeLayout yogoBtn = (RelativeLayout)findViewById(R.id.yogoBtn);
+        RelativeLayout cardioBtn = (RelativeLayout)findViewById(R.id.cardioBtn);
+
+        weightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        yogoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        cardioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
