b5d0d3f1cfbd10a34730baa537e9609f13e15ef2
bing.zhub <bing.zhub@gmail.com>
2018-08-14 12:04:58 +0800

    finish intent

--- app/src/main/java/com/example/bing/flaborfit/DetailsActivity.java
+++ app/src/main/java/com/example/bing/flaborfit/DetailsActivity.java
@@ -2,6 +2,8 @@ package com.example.bing.flaborfit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        TextView title = (TextView)findViewById(R.id.titleView);
+        ImageView image = (ImageView)findViewById(R.id.imageView);
+
     }
 }
--- app/src/main/java/com/example/bing/flaborfit/MainActivity.java
+++ app/src/main/java/com/example/bing/flaborfit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.bing.flaborfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_ITEM_TITLE = "extra.item.title";
     public static final String EXERCISE_WEIGHT = "weight";
     public static final String EXERCISE_YOGO = "yogo";
     public static final String EXERCISE_CARDIO = "cardio";
@@ -44,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadDetailActivity(String exercise_title){
-
+        Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+        intent.putExtra(MainActivity.EXTRA_ITEM_TITLE, exercise_title);
+        startActivity(intent);
     }
 }
